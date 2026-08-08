1e307416934634d8e44c1f0bb62e7cb4f161b1fc
임현빈 <leobin97@gmail.com>
2026-08-08 16:21:14 +0900

    feat: P3-T3 관리자 로그인/로그아웃 및 정보 조회 구현

---
+++ src/main/java/com/monicalab/admin/controller/AdminViewController.java
@@ -0,0 +1,13 @@
+package com.monicalab.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminViewController {
+
+    @GetMapping("/admin/login")
+    public String loginPage() {
+        return "admin/login";
+    }
+}
